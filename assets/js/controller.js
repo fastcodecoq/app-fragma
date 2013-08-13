706828bfe945373ec3a7cbaef168a5756b2277b8
gomosoft <info@gomosoft.com>
2013-08-13 00:29:18 -0500

    build 06

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -188,7 +188,55 @@ function controller(){
 }
 
 
-var con = new controller;
 
-$(con.ini);
+document.addEventListener("deviceready",onDeviceReady,false);
 
+function onDeviceReady() {
+
+          pictureSource=navigator.camera.PictureSourceType;
+          destinationType=navigator.camera.DestinationType;        
+          
+
+          
+         try{
+          
+             window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, ini, null);
+           
+           }
+           catch(e)
+           {
+
+            alert("error obteniendo sistema de archivos local");
+
+           }
+           
+
+           var con = new controller;
+               con.ini();
+
+               ini();
+
+        
+        }
+
+
+
+//-------------- iniciado 
+
+
+
+function ini(){
+
+      alert("hola")
+
+     shake.startWatch(shake);
+
+}
+
+
+
+function shake(){
+
+  alert("you shake me");
+
+}
