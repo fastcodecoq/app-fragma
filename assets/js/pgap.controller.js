cfff2cd146ab0db9c0bbe60d70b1901060a22228
gomosoft <info@gomosoft.com>
2013-11-24 08:29:10 -0500

    defining bottom bar

--- assets/js/pgap.controller.js
+++ assets/js/pgap.controller.js
@@ -74,7 +74,7 @@ var logout = function(){ window.route("login"); window.localStorage.removeItem("
  FB.Event.subscribe('auth.statusChange', loginStatusChange);      
  FB.getLoginStatus(loginStatusChange);
 
-  window.route("home");
+ window.route("home");
                                       
            } catch (e) {
                 
